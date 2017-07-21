ec29947e6b687829dd828a173b76976ce9981464
Ronny Mandal <ronnyma@transfinite.no>
2017-07-21 22:27:05 +0200

    Tidying
    Removed reduntant comments and idempotent code.

--- binarytree/avl_tree.c
+++ binarytree/avl_tree.c
@@ -18,38 +18,30 @@ void insert_avl(struct t_node **t_node, int value) {
 }
 
 void rebalance(t_node **t_node) {
-    /* Determine rotation to perform. */
+    /* Update balance factor. */
     (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
 
-    //printf("Value of node before rebalancing: %i(%i)\n", (*&(*t_node)->v), (*&(*t_node)->balance_factor));
     //If node is right heavy
     if ((*t_node)->balance_factor > 1) {
         //if right child is right heavy: rotate left
         if (((*t_node)->right) != NULL && ((*t_node)->right)->balance_factor > 0) {
             rotate_left(t_node);
-            (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
             //if right child is left heavy: half rotate right, rotate left
         } else if (((*t_node)->right) != NULL && ((*t_node)->right)->balance_factor < 0) {
             half_rotate_right(t_node);//&(*t_node)->right);
-            (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
             rotate_left(t_node);
-            (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
         }
         //If node is left heavy
     } else if ((*t_node)->balance_factor < -1) {
         //if left child is left heavy: rotate right
         if (((*t_node)->left) != NULL && ((*t_node)->left)->balance_factor < 0) {
             rotate_right(t_node);
-            (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
             //if left child is right heavy: half rotate left, rotate right
         } else if (((*t_node)->left) != NULL && ((*t_node)->left)->balance_factor > 0) {
             half_rotate_left(t_node);//&(*t_node)->left);
-            (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
             rotate_right(t_node);
-            (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
         }
-    } else {}//Do nothing
-    //printf("Value of node after rebalancing: %i(%i)\n", (*&(*t_node)->v), (*&(*t_node)->balance_factor));
+    }
 }
 
 void inspect(t_node *root) {
