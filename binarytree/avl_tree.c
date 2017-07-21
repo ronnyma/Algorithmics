3490dc413c68d2ee9edefd48a278dbf51f82081d
Ronny Mandal <ronnyma@transfinite.no>
2017-07-21 21:24:02 +0200

    Insert in AVL-tree working
    Removed statement setting pointer wrongfully to NULL

--- binarytree/avl_tree.c
+++ binarytree/avl_tree.c
@@ -1,51 +1,56 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include "avl_tree.h"
 
 void insert_avl(struct t_node **t_node, int value) {
-    if (*t_node == NULL) {
+    if (*t_node == NULL) {//TODO: handle case where value exists
         *t_node = create_node(value);
         return;
     }
 
     if (value < (*t_node)->v) {
         insert_avl(&(*t_node)->left, value);
-    } else {
+    } else if (value > (*t_node)->v) {
         insert_avl(&(*t_node)->right, value);
     }
-    //TODO: balance tree
-    //TODO: find a way to determine balance factor
     /* Update balance factor of nodes in the insertion path. */
+    rebalance(t_node);
+}
 
-    (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
+void rebalance(t_node **t_node) {
     /* Determine rotation to perform. */
+    (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
 
+    //printf("Value of node before rebalancing: %i(%i)\n", (*&(*t_node)->v), (*&(*t_node)->balance_factor));
     //If node is right heavy
     if ((*t_node)->balance_factor > 1) {
         //if right child is right heavy: rotate left
         if (((*t_node)->right) != NULL && ((*t_node)->right)->balance_factor > 0) {
             rotate_left(t_node);
+            (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
             //if right child is left heavy: half rotate right, rotate left
         } else if (((*t_node)->right) != NULL && ((*t_node)->right)->balance_factor < 0) {
             half_rotate_right(t_node);//&(*t_node)->right);
+            (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
             rotate_left(t_node);
+            (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
         }
         //If node is left heavy
     } else if ((*t_node)->balance_factor < -1) {
         //if left child is left heavy: rotate right
         if (((*t_node)->left) != NULL && ((*t_node)->left)->balance_factor < 0) {
             rotate_right(t_node);
+            (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
             //if left child is right heavy: half rotate left, rotate right
         } else if (((*t_node)->left) != NULL && ((*t_node)->left)->balance_factor > 0) {
             half_rotate_left(t_node);//&(*t_node)->left);
+            (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
             rotate_right(t_node);
+            (*&(*t_node)->balance_factor) = get_balance_factor(*t_node);
         }
-    }
-    else {}//Do nothing
-
+    } else {}//Do nothing
+    //printf("Value of node after rebalancing: %i(%i)\n", (*&(*t_node)->v), (*&(*t_node)->balance_factor));
 }
-
 
 void inspect(t_node *root) {
     if (root == NULL)
@@ -58,14 +63,15 @@ void inspect(t_node *root) {
 
 /* Return 0 if balanced, -1 if left over weight and 1 of right over weight. */
 int check_balanced(t_node *root) {
+
     int left = get_height(root->left);
     int right = get_height(root->right);
     int diff = abs(-left + right) > 1 ? 1 : 0;
 
     if (diff)
         return left > right ? -1 : 1;
-    else
-        return 0;
+
+    return 0;
 }
 
 int get_balance_factor(t_node *root) {
@@ -86,8 +92,8 @@ void rotate_right(struct t_node **t_node) {
 }
 
 void rotate_left(struct t_node **t_node) {
-    struct t_node *r = *t_node; //4
-    struct t_node *p = r->right; //7
+    struct t_node *r = *t_node;
+    struct t_node *p = r->right;
 
     r->right = p->left;
     p->left = r;
@@ -98,8 +104,8 @@ void half_rotate_right(struct t_node **t_node) {
     struct t_node *r = *t_node;
     struct t_node *p = (*t_node)->right->left;
 
+    r->right->left = p->right;
     p->right = r->right;
-    r->right->left = NULL;
     r->right = p;
 }
 
@@ -107,7 +113,7 @@ void half_rotate_left(struct t_node **t_node) {
     struct t_node *r = *t_node;
     struct t_node *p = (*t_node)->left->right;
 
+    r->left->right = p->left;
     p->left = r->left;
-    r->left->right = NULL;
     r->left = p;
 }
