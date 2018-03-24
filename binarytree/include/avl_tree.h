19efdc7d3623947998a1ebf9f70951234fcfa59d
Ronny Mandal <ronnyma@transfinite.no>
2018-03-24 23:03:17 +0100

    New include directory

---
+++ binarytree/include/avl_tree.h
@@ -0,0 +1,28 @@
+#ifndef BINARYTREE_AVL_TREE_H
+#define BINARYTREE_AVL_TREE_H
+
+#include "tree.h"
+
+void insert_avl(t_node **, int);
+
+int check_balanced(t_node *);
+
+void rebalance(t_node **t_node);
+
+void inspect(t_node *);
+
+void delete(t_node **, int);
+
+void remove_node(t_node **);
+
+void rotate_left(t_node **);
+
+void rotate_right(t_node **);
+
+void half_rotate_left(struct t_node **);
+
+void half_rotate_right(struct t_node **);
+
+int get_balance_factor(t_node *);
+
+#endif //BINARYTREE_AVL_TREE_H
---
+++ binarytree/include/binary_tree.h
@@ -0,0 +1,9 @@
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+
+#include "tree.h"
+
+/* Inserts a value into the tree. t_node is the parent node, usually the root. */
+void insert(t_node **, int);
+
+#endif
