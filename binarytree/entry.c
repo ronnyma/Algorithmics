34aaa4bd3c79b4a5b6565b6a9788f3e56a7e692f
Ronny Mandal <ronnyma@transfinite.no>
2018-03-24 23:28:40 +0100

    Working delete with nodes having zero or one child.

--- binarytree/entry.c
+++ binarytree/entry.c
@@ -6,7 +6,7 @@ int main() {
 
 
     int val[] = {5, 4, 7, 23, 9, 53, 2, 45, 71, 22, 82, 87, 15, 1, 16, 21, 93, 29, 120, 84, 10, 3, 44};
-    int var[] = {15};
+    int var[] = {15, 2, 45, 71, 22, 82, 87, 1, 16, 21, 93, 29, 120};
 
     /* Initialize the root. */
     struct t_node *root = initialize(val[0]);
