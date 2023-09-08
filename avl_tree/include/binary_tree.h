98b041e64e0887d14ebc49dd848258ad6e927e9b
Ronny Mandal <ronnyma@transfinite.io>
2023-09-09 01:20:25 +0200

    Init

---
+++ avl_tree/include/binary_tree.h
@@ -0,0 +1,11 @@
+#include "tree.h"
+
+
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+
+
+/* Inserts a value into the tree. node_t is the parent node, usually the root. */
+void insert(node_t **, int);
+
+#endif
